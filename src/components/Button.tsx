064a432a74cd05870686225016c9f6edb6ec622e
[dyad] <git@dyad.sh>
2026-07-08 22:45:54 -0300

    [dyad] Ajuste de botões premium, menu lateral colapsável e novos filtros na tela de lançamentos.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,7 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-  className?: string;
-}
-
-export default function Button({ children, className = '', ...props }: ButtonProps) {
+export default function Button({ children, className = '', ...props }: React.ComponentProps<"button">) {
   return (
     <StyledWrapper className={className}>
       <button className="Btn" {...props}>
