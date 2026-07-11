60c812d1f972bc44592a491979078d5376548d58
[dyad] <maiconeleuterio@gmail.com>
2026-07-11 17:55:39 -0300

    [dyad] (1 files changed)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { useLocation, useNavigate } from 'react-router-dom';
 import {
   BarChart3,
