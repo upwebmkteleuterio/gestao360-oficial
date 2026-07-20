216284d9aa777b0d79aa1dd5ed1111ffd01a22ea
[dyad] <maiconeleuterio@gmail.com>
2026-07-20 13:40:31 -0300

    Reverted all changes back to version f58abb159402a7d69377153ae8dda10964429346

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import ComprovanteDrawer from './components/ComprovanteDrawer';
 import CadastroRapidoModal from './components/CadastroRapidoModal';
 import BaixaLancamentoModal from './components/BaixaLancamentoModal';
 import AprovarLancamentoModal from './components/AprovarLancamentoModal';
+import AITestSuite from './components/AITestSuite';
 
 import NewLaunchButton from './components/NewLaunchButton';
 
@@ -224,6 +225,7 @@ function AppContent() {
               <CadastroRapidoModal />
               <BaixaLancamentoModal />
               <AprovarLancamentoModal />
+              <AITestSuite />
             </div>
           </PrivateRoute>
         }
